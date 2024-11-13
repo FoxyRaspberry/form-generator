3fa68bcf8bdbc89345b5eb04017781d13fb00570
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2024-11-13 23:38:14 +0300

    feat (test-select): provide the ability to embed in a form

--- src/app/test-select/test-select.component.spec.ts
+++ src/app/test-select/test-select.component.spec.ts
@@ -1,23 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TestSelectComponent } from './test-select.component';
 
-describe('TestSelectComponent', () => {
+describe('TestSelectComponent', (): void => {
   let component: TestSelectComponent;
   let fixture: ComponentFixture<TestSelectComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async (): Promise<void> => {
     await TestBed.configureTestingModule({
-      imports: [TestSelectComponent]
+      imports: [TestSelectComponent],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(TestSelectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', (): void => {
     expect(component).toBeTruthy();
   });
 });
