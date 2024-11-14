199d49e928b71aba109b2bb73922c073508e18ba
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2024-11-14 16:56:50 +0300

    feat (test-input): provide the ability to embed in a form

--- src/app/test-input/test-input.component.ts
+++ src/app/test-input/test-input.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
+  imports: [FormsModule],
+  providers: [
+    {
+      multi: true,
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TestInputComponent),
+    },
+  ],
   selector: 'app-test-input',
   standalone: true,
-  imports: [],
+  styleUrl: './test-input.component.scss',
   templateUrl: './test-input.component.html',
-  styleUrl: './test-input.component.scss'
 })
-export class TestInputComponent {
+export class TestInputComponent implements ControlValueAccessor {
+  protected disabled = false;
 
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+  private valueInternal = '';
+
+  protected get value(): string {
+    return this.valueInternal;
+  }
+
+  protected set value(value: string) {
+    this.writeValue(value);
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+
+  public writeValue(value: string | null): void {
+    if (value === null) {
+      return;
+    }
+
+    this.valueInternal = value;
+    this.onChange(this.valueInternal);
+  }
 }
