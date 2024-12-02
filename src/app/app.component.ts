de2d1997f389f829873926c05712e2939b8ebc5e
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2024-12-03 00:02:18 +0300

    style (app): create the styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilderComponent } from './form-renderer/form-builder/form-builder.component';
+import { FormConfigurationEditorComponent } from './form-manager/form-configuration-editor/form-configuration-editor.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    // TODO: Learn `RouterOutlet`.
+    RouterOutlet,
+    
+    FormBuilderComponent,
+    FormConfigurationEditorComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'form-generator';
-}
+export class AppComponent {}
